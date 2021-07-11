acdb912d0a17f6051f15d84fc5506f9311b86ef9
agyanatma <agyanatma1410@gmail.com>
2021-07-11 22:56:52 +0700

    update responsive layanan section

--- components/templates/Beranda/LayananSection/Layanansection.styles.tsx
+++ components/templates/Beranda/LayananSection/Layanansection.styles.tsx
@@ -20,14 +20,28 @@ export const LayananContainer = styled(Container)`
 export const LayananContent = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
     column-gap: 40px;
+
+    ${media.lessThan("large")`
+        grid-template-columns: 1fr;
+
+        & ${LayananCardWrapper} {
+            width: 60%;
+            margin: 20px auto;
+        }
+
+        & ${LayananCardPrice} {
+            width: 70%;
+            margin: 16px auto;
+        }
+    `}
 
     ${media.lessThan("medium")`
         grid-template-columns: 1fr;
 
         & ${LayananCardWrapper} {
-            width: 60%;
+            width: 80%;
             margin: 20px auto;
         }
 
